df38f2328e489651c7175706456c109a8395eae2
Nguyen Thanh Trieu <trieunguyenthanh@admicro.vn>
2020-06-10 23:04:56 +0700

    update code

---
+++ App/controller/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\controller;
+
+class HomeController
+{
+	public function index()
+	{
+		echo "This is home controller";
+	}
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+// khong duoc phep truy cap truc tiep vao file
+if(!defined('ROOT_PATH')){
+	die('Can not access');
+}
+
+// la noi tiep nhan cac request gui len
+// index.php?c=home&m=index&name=test&age=100
+// index.php/home/index
+
+// c : ten controller
+// m : ten phuong thuc thuoc controller do
+// name + age : query string
+$controller = ucfirst($_GET['c'] ?? 'home');
+$method = $_GET['m'] ?? 'index';
+
+$namespaceController = "App\controller\\".$controller."Controller";
+
+$checkExist = str_replace('\\', '/', trim($namespaceController,'\\')) . ".php";
+
+if(file_exists($checkExist)){
+	$c = new $namespaceController;
+	// tu dong goi vao cac phuong thuc
+	$c->$method();
+} else {
+	header("Location: error.php");
+}
